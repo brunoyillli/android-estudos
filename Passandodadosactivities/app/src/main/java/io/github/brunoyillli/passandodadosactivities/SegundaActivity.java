35fd76e593246e35a92d26d4781a4aa4268478dd
Bruno Mendes <wolfteam1910@gmail.com>
2022-11-18 16:46:41 -0300

    estudo sobre mudança de telas e passagem de dados

---
+++ Passandodadosactivities/app/src/main/java/io/github/brunoyillli/passandodadosactivities/SegundaActivity.java
@@ -0,0 +1,30 @@
+package io.github.brunoyillli.passandodadosactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    private TextView textNome, textIdade, textEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+
+        textNome = findViewById(R.id.textNome);
+        textIdade = findViewById(R.id.textIdade);
+        textEmail = findViewById(R.id.textEmail);
+
+        Bundle dados = getIntent().getExtras();
+        String nome = dados.getString("nome");
+        int idade = dados.getInt("idade");
+        Usuario usuario = (Usuario) dados.getSerializable("objeto");
+
+        textNome.setText(nome);
+        textIdade.setText(String.valueOf(idade));
+        textEmail.setText(usuario.getEmail());
+    }
+}
