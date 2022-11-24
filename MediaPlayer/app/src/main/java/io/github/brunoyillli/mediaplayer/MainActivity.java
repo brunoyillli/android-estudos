8a923f3666b9306117ac9467315e1e6883fcaa4d
Bruno Mendes <wolfteam1910@gmail.com>
2022-11-24 15:43:36 -0300

    estudo manipulando audio

---
+++ MediaPlayer/app/src/main/java/io/github/brunoyillli/mediaplayer/MainActivity.java
@@ -0,0 +1,95 @@
+package io.github.brunoyillli.mediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private SeekBar seekVolume;
+    private AudioManager audioManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        generateMusic();
+        inicilizarSeekBar();
+
+    }
+
+    private void inicilizarSeekBar(){
+        seekVolume = findViewById(R.id.seekVolume);
+
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int volumeMaximo = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int volumeAtual = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        seekVolume.setMax(volumeMaximo);
+        seekVolume.setProgress(volumeAtual);
+
+        seekVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i,
+                        AudioManager.FLAG_PLAY_SOUND);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+    }
+
+    private void generateMusic() {
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bach);
+    }
+
+    public void executarSom(View view){
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+        }
+    }
+
+    public void pausarMusica(View view){
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        }
+    }
+
+    public void pararMusica(View view){
+        if( mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            generateMusic();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if( mediaPlayer != null && mediaPlayer.isPlaying() ){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        }
+    }
+}
