cc714da967995f258884b66c83a774072e5e8be4
Bruno Mendes <wolfteam1910@gmail.com>
2022-12-12 08:53:24 -0300

    voltando a estudar trabaio tava puxado

--- FisebaseApp/app/src/main/java/io/github/brunoyillli/fisebaseapp/MainActivity.java
+++ FisebaseApp/app/src/main/java/io/github/brunoyillli/fisebaseapp/MainActivity.java
@@ -1,11 +1,16 @@
 package io.github.brunoyillli.fisebaseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
 
         DatabaseReference usuarios = reference.child("usuarios");
         DatabaseReference produtos = reference.child("produtos");
+
+        usuarios.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.i("FIREBASE", snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         /*
         Usuario usuario = new Usuario();
         usuario.setNome("Mayara Cristina");
@@ -26,13 +43,16 @@ public class MainActivity extends AppCompatActivity {
         usuario.setIdade(21);
 
         usuarios.child("2").setValue(usuario);
-        */
+
         Produto produto = new Produto();
         produto.setDescricao("Redmi Note 8");
         produto.setMarca("Xiaomi");
         produto.setPreco(1020.36);
 
         produtos.child("1").setValue(produto);
+
+         */
+
 
     }
 }
